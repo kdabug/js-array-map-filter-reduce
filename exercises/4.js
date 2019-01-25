d4a3db573826ed899717fddb832ae3ba0c3e8ed1
Mikayda Mills <mills.ma12@gmail.com>
2019-01-25 16:17:27 -0500

    finished ex 4 fruitColor

--- exercises/4.js
+++ exercises/4.js
@@ -2,4 +2,20 @@
 // using .map() that "maps" the fruit to its color.
 // For example, ['cherry', 'lemon'] would turn into ['red', 'yellow']
 
-const fruits = ['banana', 'strawberry', 'strawberry', 'banana', 'strawberry'];
+const fruits = ["banana", "strawberry", "strawberry", "banana", "strawberry"];
+
+const fruitColor = fruits.map(fruit => {
+  if (fruit === "banana") {
+    fruit = "yellow";
+  } else {
+    fruit = "red";
+  }
+  return fruit;
+});
+
+console.log(fruitColor);
+
+const fruitColor2 = fruits.map(fruit =>
+  fruit === "bananas" ? "red" : "yellow"
+);
+console.log(fruitColor2);
