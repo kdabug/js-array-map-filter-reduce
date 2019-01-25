5eb8684460744c9ea5e038762275f924c175d710
Mikayda Mills <mills.ma12@gmail.com>
2019-01-25 16:02:10 -0500

    finished ex. 1 'bananas'

--- exercises/1.js
+++ exercises/1.js
@@ -1,6 +1,7 @@
-// Given an array of fruits, make a new array called `bananas` 
+// Given an array of fruits, make a new array called `bananas`
 // with the .filter() method that only contains bananas.
 
-const fruits = ['banana', 'strawberry', 'strawberry', 'banana', 'strawberry'];
+const fruits = ["banana", "strawberry", "strawberry", "banana", "strawberry"];
+const bananas = fruits.filter(fruit => fruit === "banana");
 
-console.log('hi');
+console.log(bananas);
